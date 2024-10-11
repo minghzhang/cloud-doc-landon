30ea5f712ffa288ce9024990c7111996d57e2231
landonzhang <landon.zhang@zoom.us>
2024-10-10 18:25:10 -0700

    combine React with Electron

---
+++ main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron');
+//const isDev = require('electron-is-dev');
+let mainWindow;
+const { exec } = require('child_process'); // 用于终止进程
+
+app.whenReady().then(() => {
+    // 使用动态导入加载 ES Module
+    const isDev = true;
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 680,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    })
+
+    const urlLocation = isDev ? 'http://127.0.0.1:3000' : 'http://dummyUrl'
+    mainWindow.loadURL(urlLocation);
+
+    mainWindow.on('closed', () => {
+        // Electron 窗口关闭时，终止 React 开发服务器
+        if (isDev) {
+            exec('lsof -t -i :3000 | xargs kill', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error killing process: ${error}`);
+                    return;
+                }
+                console.log(`Server on port 3000 stopped`);
+            });
+        }
+        app.quit();
+    });
+})
