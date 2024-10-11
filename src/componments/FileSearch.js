be549abccb83d1e4ae8342c6d6ad6874acb5507b
landonzhang <landon.zhang@zoom.us>
2024-10-10 22:32:54 -0700

    FileSearch v3 add FontawesomeIcon

--- src/componments/FileSearch.js
+++ src/componments/FileSearch.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch, faTimes} from '@fortawesome/free-solid-svg-icons';
 
 const FileSearch = ({title, onFileSearch}) => {
     const [inputActive, setInputActive] = useState(false);
@@ -40,23 +42,19 @@ const FileSearch = ({title, onFileSearch}) => {
                 {!inputActive &&
                     <div className="d-flex justify-content-between align-items-center">
                         <span className="">{title}</span>
-                        <button type="button" className="btn btn-primary" onClick={() => setInputActive(true)}>
-                            search
+                        <button type="button" className="icon-button" onClick={() => setInputActive(true)}>
+                            <FontAwesomeIcon icon={faSearch} size="lg"/>
                         </button>
                     </div>
                 }
                 {inputActive &&
-                    <div className="row">
-                        <div className="col-8">
-                            <input type="text" className="form-control" value={value} ref={node}
-                                   onChange={(e) => setValue(e.target.value)}/>
-                        </div>
-                        <div className="col-4">
-                            <button type="button" className="btn btn-primary"
-                                    onClick={closeSearch}>
-                                close
-                            </button>
-                        </div>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <input type="text" className="form-control" value={value} ref={node}
+                               onChange={(e) => setValue(e.target.value)}/>
+
+                        <button type="button" className="icon-button" onClick={closeSearch}>
+                            <FontAwesomeIcon icon={faTimes} size="lg"/>
+                        </button>
                     </div>
                 }
             </div>
